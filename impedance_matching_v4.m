ba18f96fa04772f8e3e78ce8b900651448f3bbaa
Morgan Roberts <morgan.roberts.18@ucl.ac.uk>
2018-10-16 11:06:51 +0100

    Tidying up figure 1 and changing legend entries for readability

--- impedance_matching_v4.m
+++ impedance_matching_v4.m
@@ -20,7 +20,6 @@
 %
 %% Section 1: Defining Parameters.
 
-close all
 TRANSDUCER_IMPEDANCE = 5.9996 - 5.9771j;   % [ohms]
 GENERATOR_IMPEDANCE  = 50 + 0j;            % [ohms]
 FREQUENCY            = 1e6;                % [Hz]
@@ -28,39 +27,46 @@ GENERATOR_AMPLITUDE  = 50;                 % [V]
 GENERATOR_PHASE      = 0;                  % [rad]
 w                    = 2 * pi * FREQUENCY; % [rad/s] 
 
-%% Section 2: Generating time series and generator voltage waveforms.
+%% Section 2: Generating time series and Generator voltage waveforms.
 
-t  = 0:1e-8:2e-6;        % Time data
+% We generate data for two cycles.
+t  = 0:1e-8:2 / FREQUENCY;
 vg = real(GENERATOR_AMPLITUDE * exp(1j * (w * t - GENERATOR_PHASE)));
 
 %% Section 3: Performance for direct connection (no matching network).
 
-% Use series combination of transducer and generator impedances to
-% calculate generator current waveform ig(t).
+% Use series combination of Transducer and Generator impedances to
+% calculate Generator current waveform ig(t).
 ig = real((GENERATOR_AMPLITUDE / abs(GENERATOR_IMPEDANCE + ...
     TRANSDUCER_IMPEDANCE)) * exp(1j * (w * t - GENERATOR_PHASE - ...
     angle(GENERATOR_IMPEDANCE + TRANSDUCER_IMPEDANCE))));
 
-% Calculate resistive (R) and reactive (X) components of transducer and 
-% generator impedances.
+% Calculate resistive (R) and reactive (X) components of Transducer and 
+% Generator Impedances.
 Rt = real(TRANSDUCER_IMPEDANCE); 
 Xt = imag(TRANSDUCER_IMPEDANCE); 
 Rg = real(GENERATOR_IMPEDANCE); 
 Xg = imag(GENERATOR_IMPEDANCE); 
 
-% Calculate average power delivered by generator for direct connection
-Pt = (GENERATOR_AMPLITUDE^2 / 2) * Rt / ((Rg + Rt)^2 + (Xg + Xt)^2);
-fprintf('Unmatched average power delivered = %.3f watts.\n',Pt);
+% Calculate and display average Power delivered by Generator for direct
+% connection.
+P = (GENERATOR_AMPLITUDE^2 / 2) * Rt / ((Rg + Rt)^2 + (Xg + Xt)^2);
+fprintf('Unmatched average power delivered = %.3f watts.\n', P);
 
-%% Section 4: Designing the conjugate-matched network
+% Calculate and display phase offset between generator Voltage and Current.
+fprintf('Unmatched current phase offset = %.2f radians.\n', ...
+                      - angle(GENERATOR_IMPEDANCE + TRANSDUCER_IMPEDANCE));
 
-% We model the combination of the signal generator and the L-C matching
+%% Section 4: Designing the conjugate-matched network.
+
+% We model the combination of the Generator and the L-C matching
 % network as a 'Voltage Source'. 
 % Using source transformations, we determine the Thevenin equivalent 
 % Voltage, vs(t), and Impedance, Zs, for this 'Voltage Source'.
 
-% We can derive values of C and L for which the Thevenin equivalent 
-% Impedance is the complex conjugate of the Transducer Impedance.
+% We can derive values of C and L for which the Thevenin equivalent
+% 'Voltage Source' Impedance is the complex conjugate of the Transducer 
+% Impedance.
 C = sqrt((Rg - Rt) / (Rt * Rg^2 * w^2));
 L = (Rg^2 * w * C - Xt * (Rg^2 * w^2 * C^2 + 1)) / (w + Rg^2 * w^3 * C^2);
 
@@ -68,7 +74,7 @@ L = (Rg^2 * w * C - Xt * (Rg^2 * w^2 * C^2 + 1)) / (w + Rg^2 * w^3 * C^2);
 ZC = 1 / (1j * w* C); 
 ZL = 1j * w * L;
 
-% We calculate the Thevenin equivalent Impedance of the source, with its
+% We calculate the Thevenin equivalent 'Voltage Source' Impedance, with its
 % resistive (Rs) and reactive (Xs) components.
 Zs = ZL + (Rg * ZC) / (Rg + ZC);
 Rs = real(Zs); 
@@ -76,37 +82,66 @@ Xs = imag(Zs);
 
 % We calculate the Thevenin equivalent Voltage waveform vs(t), and its
 % amplitude, Vs.
-vs = real(Vg * abs(ZC / (Rg + ZC)) * exp(1j * (w * t - GENERATOR_PHASE ...
-                                                + angle(ZC / (Rg + ZC)))));
-Vs = max(vs);
+Vs = GENERATOR_AMPLITUDE * abs(ZC / (Rg + ZC));
+vs = real(Vs * exp(1j * (w * t - GENERATOR_PHASE + angle(ZC / ...
+                                                             (Rg + ZC)))));
 
-%%
-% To check that the power delivered to the transducer has been maximised,
-% we will determine the phase difference between the voltage and the
-% current, and calculate the maximum average power delivered to the load.
+%Vs = max(vs)
 
+%% Section 5: Performance for conjugate-matched network.
 
-is = real((Vg*abs(ZC/(Rg + ZC))/abs(Zs + TRANSDUCER_IMPEDANCE))*exp(j*(w*t - GENERATOR_PHASE + angle(ZC/(Rg + ZC)) - angle(Zs + TRANSDUCER_IMPEDANCE))));
+% Calculate average power delivered by generator for direct connection
+Pmatch = (Vs^2 / 2) * Rt / ((Rs + Rt)^2 + (Xs + Xt)^2);
+fprintf('Conjugate matched average power delivered = %.3f watts.\n',...
+                                                                   Pmatch);
 
+%% Section 6: Phase comparison for matched and unmatched arrangements.
+
+% Use series combination of transducer and Thevenin equivalent 'Voltage 
+% Source' impedances to calculate 'Voltage Source' current waveform is(t).
+is = real((Vs / abs(Zs + TRANSDUCER_IMPEDANCE)) * exp(1j * (w * t - ...
+                   GENERATOR_PHASE + angle(ZC / (Rg + ZC)) - angle(Zs + ...
+                                                  TRANSDUCER_IMPEDANCE))));
+
+% We plot the Generator Voltage and Current waveforms for the unmatched
+% arrangement to visualise the phase offset.
+
+% Set both y-axis colours to black.
+close all
 fig1 = figure(1);
-left_color = [0 0 0];
-right_color = [0 0 0];
+left_color = [0, 0, 0];
+right_color = [0, 0, 0];
 set(fig1,'defaultAxesColorOrder',[left_color; right_color]);
+
+% Voltage data on left y-axis.
 yyaxis left
 hold on
-plot(t,vg,'k'); ylabel('Volts [V]'); ylim([-max([vs vg])*1.2 max([vs vg])*1.2]);
-[pkvg,tvg] = findpeaks(vg);
-stem(t(tvg(1)),pkvg(1),'k-')
- 
-yyaxis right
-plot(t,ig,'k--'); ylabel('Current [A]');
+h1 = plot(t, vg, 'k'); 
+ylabel('Volts [V]'); 
+ylim([- max([vs, vg]) * 1.2, max([vs, vg]) * 1.2]);
 
-xlabel('Time [s]'); ylim([-max([is ig])*1.2 max([is ig])*1.2]); 
-title('Signal Generator v_g(t) and i_g(t) for un-matched circuit');
+% Finding and plotting peak to mark the Generator Voltage amplitude. 
+[pkvg, tvg] = findpeaks(vg);
+stem(t(tvg(1)), pkvg(1), 'k-');
+
+% Current data on right y-axis.
+yyaxis right
+h2 = plot(t,ig,'k--');
+ylabel('Current [A]');
+ylim([- max([is, ig]) * 1.2, max([is, ig]) * 1.2]); 
+
+% Finding and plotting peak to mark the Generator Current amplitude. 
 [pkig,tig] = findpeaks(ig);
-stem(t(tig(1)),pkig(1),'k--')
-legend('Sig. Gen. Voltage v_g(t)','V_g','Sig. Gen. Current i_g(t)','I_g');
+stem(t(tig(1)), pkig(1), 'k--')
+
+% Adding labels, title, and legend.
+xlabel('Time [s]'); 
+title('Signal Generator v_g(t) and i_g(t) for un-matched circuit');
+legend([h1, h2], {'Generator Voltage v_g(t)', 'Generator Current i_g(t)'});
 hold off
+
+% We repeat by plotting the Generator Voltage and Current waveforms for 
+% the unmatched arrangement to verify that the phase offset is zero.
 
 fig2 = figure(2);
 left_color = [0 0 0];
@@ -119,9 +154,9 @@ hold on
 plot(t,is,'k--'); ylim([-max([is ig])*1.2 max([is ig])*1.2]);
 ylabel('Current [A]'); legend('Source Voltage v_s(t)','Source Current i_s(t)'); 
 xlabel('Time [s]'); title('Source v_s(t) and i_s(t) for conjugate-matched Thevenin equivalent circuit');
-Ptmatch = (Vs^2/2)*Rt/((Rs + Rt)^2 + (Xs + Xt)^2); % Average power delivered to transducer
 
-fprintf('Unmatched current phase angle =  %.2f radians.\n',-angle(GENERATOR_IMPEDANCE + TRANSDUCER_IMPEDANCE));
 
-fprintf('Conjugate matched average power delivered = %.3f watts.\n',Ptmatch);
+
+
+
 
